0c3b4ec7aec24f625b74d295cae669dcbe464aa2
Yuxuan <ychen14@stuy.edu>
2016-09-25 17:27:25 -0400

    update newer version

--- hw03.py
+++ hw03.py
@@ -4,23 +4,24 @@ hw03 = Flask(__name__)
 
 jobs = dict()
 joblist = list()
-
+num = 0.0
 percentile = 0.0
+
 with open('occupations.csv','r') as csvfile:
     f = csv.reader(csvfile)
     for row in f:
         if row[0] != 'Job Class' and row[0] != 'Total':
+            start = percentile
             percentile += float(row[1])
-            val = [float(row[1]),percentile]
+            val = [float(row[1]),start,percentile]
             jobs[row[0]] = val
             joblist.append(row)
             
 def jobPicker():
     num = float(random.random()*percentile)
     for job in jobs:
-        if num <= jobs[job][1]:
-            print(job)
-            return
+        if num >= jobs[job][1] and num < jobs[job][2]:
+            return job
 
 @hw03.route("/")
 def welcome():
@@ -28,7 +29,8 @@ def welcome():
 
 @hw03.route("/occupations")
 def generate():
-    return render_template("table.html", l=joblist, job=jobPicker())
+    j = jobPicker()
+    return render_template("table.html", l=joblist, job=j, per=num)
 
 
 if __name__ == "__main__":
